64f61cadbaccade194ce9c19df1cfcaa09b80327
nichiluan <nichiluan@bytedance.com>
2019-03-21 17:20:43 +0800

    feat: update lakawei_rpc
    Change-Id: I22088cd43bcec4a0b43220c366780bc2d4b300c1

--- vendor/github.com/nclgh/lakawei_rpc/server/rpc.go
+++ vendor/github.com/nclgh/lakawei_rpc/server/rpc.go
@@ -27,8 +27,8 @@ func Init() {
 
 func Run(rcvr interface{}) error {
 	rpc.Register(rcvr)
-	tcpAddr, err := net.ResolveTCPAddr("tcp4", fmt.Sprintf("%s:%s", ServiceMountIp, ServicePort))
-	logrus.Infof("Service to listening tcp service %s %s:%s", ServiceName, ServiceMountIp, ServicePort)
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", ":"+ServicePort)
+	logrus.Infof("Service to listening tcp service %s port:%s", ServiceName, ServicePort)
 
 	if err != nil {
 		panic(err)
